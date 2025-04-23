d715fa8133a6b4d889c4d664f40529146cfbff11
HardRaple <github.com/hardraple>
2025-04-23 21:00:12 +0300

    ver 1.0

--- README.md
+++ README.md
@@ -1,46 +1,45 @@
-![TurboArch](artwork.png)
-
-# TurboArch - Seamlessly Replace Your Current OS with Arch Linux NOW!
-This script allows you to replace any Linux distribution with Arch Linux, preserving user data and some system configuration. 
-
-[Video demonstration on YouTube](https://youtu.be/OCnaunwBoWw) 
+# Archify - Seamlessly replace your current distribution with Arch Linux
+This script allows you to replace any Linux distribution with Arch Linux, preserving user data and providing extensive configuration options for the new system. The script will also be useful if your VPS provider allows system changes but does not provide Arch Linux image or custom image uploads.
 
 ## Quick start
 Download and run required scripts:
 ```
-git clone https://github.com/evgvs/turboarch
-cd turboarch
-sudo bash turboarch.sh
+git clone https://github.com/hardraple/archify
+cd archify
+sudo bash archify.sh
 ```
-This will ask some questions and begin Arch Linux installation. Data in `/home`, disk configuration (even with LVM and LUKS!), user accounts and passwords, sudo/wheel group will be preserved. It is also possible to automatically install the best desktop environment (GNOME) into the new system.
+This will ask some questions and begin Arch Linux installation. Data in `/home`, disk configuration (even with LVM and LUKS!), user accounts and passwords, sudo/wheel group will be preserved. Additionally, you can install one of many graphical environments, choose graphics driver and audio server. It is also possible to use a configuration file to save parameters and automation.
 
 > [!IMPORTANT]
-> Everything in `/bin`, `/boot`, `/etc`, `/lib`, `/lib64`, `/sbin`, `/srv`, `/usr` and `/var` will be permanently removed. Other directories will not be affected at all, and no partitions will be formatted.
+> Everything in `/bin`, `/boot`, `/etc`, `/lib`, `/lib64`, `/sbin`, `/srv`, `/usr` and `/var` will be permanently removed. Make a backup of the files you need from these folders, such as configuration files and virtual machine snapshots. Other directories will not be affected at all, and no partitions will be formatted.
 
 ## Requirements
 * Internet connection
 * x86_64 Linux kernel version suitable for glibc in Arch Linux (3.2+ for glibc version 2.39)
 * `grep`, `coreutils` and `util-linux`
 * `bash` version 4+
-* `wget` or `curl` and to download Arch Linux bootstrap, `tar` and `xz` to decompress bootstrap archive. You can also manually download and extract [bootstrap archive](https://geo.mirror.pkgbuild.com/iso/latest/) to `/archlinux-bootstrap`
+* `wget` or `curl` and to download Arch Linux bootstrap, `tar` and `zstd` to decompress bootstrap archive. You can also manually download and extract [bootstrap archive](https://geo.mirror.pkgbuild.com/iso/latest/) to `/archlinux-bootstrap`
 
 ## Installation process explained
-### Stage 1 (turboarch.sh)
+### Stage 1 (archify.sh)
 * The script copies needed files to safe place where they will not be removed
-* Downloads Arch Linux rootfs and extract it to `/archlinux-bootstrap`
+* Downloads Arch Linux rootfs and extracts it to `/archlinux-bootstrap`
 * Recursively mounts root to directory `host-system` inside `/archlinux-bootstrap` (`mount --rbind / /archlinux-bootstrap/host-system`)
 * Mounts `/sys`, `/dev` and `/proc` to corresponding directories inside `/archlinux-bootstrap`
 * Copies `stage2.sh` to `/archlinux-bootstrap`, chroots and runs the script
+
 ### Stage 2
 * Removes everything in `/host-system/{bin, boot, etc, lib...}`
 * Installs the base system with `pacstrap`
 * Copies fstab and users configuration to new system
 * Chroots to `/host-system` and runs `stage3.sh`
+
 ### Stage 3
 * Configures locale, hostname and time
-* Installs kernel and generate initramfs
+* Installs kernel and generates initramfs
+* Installs chosen graphics driver, sound server, graphical environment
 * Installs and configures GRUB
-* Installs GNOME and perform post-install configuration
+* Reboots or redirects you to bash
 
 ## FAQ & Fine tuning
 
@@ -48,21 +47,25 @@ This will ask some questions and begin Arch Linux installation. Data in `/home`,
 Edit the `mirrorlist.default` file to change the mirrors. If reflector is enabled, that mirrors will only be used to download python and reflector. 
 
 ### SSH
-Installing via ssh seems like a bad idea. However, it will probably work. Script will detect `$SSH_CONNECTION` and proceed to installation. If you are running script from some another remote shell, set environment variable `FORCE_NO_OPENVT=1`.
+Installing via ssh seems like a bad idea. However, it will probably work but you will have to setup ssh manually after installation. Script will detect `$SSH_CONNECTION` and proceed to installation. If you are running script from some another remote shell, set environment variable `FORCE_NO_OPENVT=1`.
 
 ### Preconfigure
-To preconfigure the script, create a file called `config.default`:
+To preconfigure the script, create a file called `config.default`, here's an example:
 ```
-GNOME=1
+COPY_USER_CONFIGURATION=1
+GRAPHIC="gnome"
 SET_SPACE_PASSWORD=1
-SRAKUT=0
+NEW_NONSPACE_PASSWORD="Will be overriden by 1) COPY_USER_CONFIGURATION 2) SET_SPACE_PASSWORD"
+DRACUT=0
 NETWORKMANAGER=1
-LOCALTIME=Europe/Moscow
-NEWHOSTNAME=archlinux
+LOCALTIME="Europe/Monaco"
+NEWHOSTNAME="archify"
 REFLECTOR=1
+GRAPHICS_DRIVER="all-open"
+SOUND_SERVER="no"
 FORCE_REBOOT_AFTER_INSTALLATION=1
 ```
-If `SET_SPACE_PASSWORD` is set to 1, root password will be ' ', i. e. space. Otherwise, script will try to copy user configuration from files `wheel_users`, `passwd_delta`, `shadow_delta`, `group_delta`, `gshadow_delta`. 
+If `COPY_USER_CONFIGURATION` is set to 1, script will try to copy user configuration from files `wheel_users`, `passwd_delta`, `shadow_delta`, `group_delta`, `gshadow_delta` and any password parameters will be ignored. If `SET_SPACE_PASSWORD` is set to 1 and `COPY_USER_CONFIGURATION` is set to 0, root password will be ' ', i. e. space and `NEW_NONSPACE_PASSWORD` will be ignored. If `COPY_USER_CONFIGURATION` and `SET_SPACE_PASSWORD` are set to 0, then `NEW_NONSPACE_PASSWORD` value will be used as root password.
 
 `wheel_users` contains list of users (one per line) that will be added to `wheel` group. This implies creation of `%wheel ALL=(ALL:ALL) ALL` rule in sudoers.
 
@@ -75,7 +78,8 @@ pimp:$y$j9T$LuhFnrcezH0TUA1GaUqwa/$Ptme335MkS61UIlNQ.0jRD1doz7zQFzKIQcP6MXW6O1:1
 ```
 
 ### Tested distributions
-Turboarch will work on almost any Linux system, because it has very few dependencies. It successfully replaced following distributions with Arch Linux:
+Сhanges in Archify do not affect compatibility with distributions. Archify, as Turboarch, works successfully on the following distributions:
+
 * Manjaro
 * Debian
 * Ubuntu
@@ -89,16 +93,22 @@ Turboarch will work on almost any Linux system, because it has very few dependen
 * Tiny Core
 
 ### I have /home, /var, /tmp, /lib, /usr/X11R6... on different partitions!
-Turboarch basically just doesn't care about your partition scheme. All mountpoints will be transferred to the new system. 
+Archify basically just doesn't care about your partition scheme. All mountpoints will be transferred to the new system. 
 
 ### LVM & LUKS
-It is supported, but if LVM or HRUKS is detected, turboarch will use SRACUT to generate initramfs. However, hooks for pacman will be installed, so SRACUT shouldn't be a big problem.
+It is supported, but if LVM or LUKS is detected, Archify will use DRACUT to generate initramfs. However, hooks for pacman will be installed, so DRACUT shouldn't be a big problem.
 
 ### ZFS
 No, it is currently not supported.
+
+### Graphics drivers
+Select the driver that is intended for the GPU that is currently displaying the image. If necessary, you can install other drivers yourself after switching to Arch Linux.
 
 ### GRUB
 On BIOS, GRUB will be installed to device that has partition that is mounted to `/boot` or `/`. On UEFI, GRUB will be installed to `/boot` or `/boot/efi`. OS-PROBER will be enabled by default.
 
 ### Why locale is set to en_US.UTF-8?
-Because you should never use any language other than English in any interface on any of your devices.
+Other locale requires fonts to be configured and this can cause problems with displaying characters in various parts of the system. In linux it is generally better to use only this locale to avoid errors.
+
+
+Archify is a fork of Turboarch. For additional information, see [Turboarch github page](https://github.com/evgvs/turboarch)
--- stage3.sh
+++ stage3.sh
@@ -2,66 +2,94 @@
 
 echo -e "\e[1m\e[46m\e[97mSTAGE 3 ACTIVATED\e[0m"
 
-if [ -f /turboarch-config/wheel_users ]; then
+if [ -f /archify-config/wheel_users ]; then
   while IFS="" read -r p || [ -n "$p" ]
   do
     echo -e "\e[1m\e[46m\e[97mADD USER $p TO GROUP wheel\e[0m"
     usermod -a -G wheel "$p"
-  done < /turboarch-config/wheel_users
+  done < /archify-config/wheel_users
 
   echo "%wheel ALL=(ALL:ALL) ALL" > /etc/sudoers.d/00_wheel
 fi
 
-if [ -f /turboarch-config/passwd_delta ]; then
+if [ -f /archify-config/passwd_delta ]; then
   while IFS="" read -r p || [ -n "$p" ]
   do
     IFS=':' read -r -a arr <<< "$p"
     echo -e "\e[1m\e[46m\e[97mCHOWN HOME DIRECTORY ${arr[5]} FOR USER ${arr[0]}\e[0m"
     chown -R "${arr[0]}:${arr[0]}" "${arr[5]}" 
-  done < /turboarch-config/passwd_delta
+  done < /archify-config/passwd_delta
 fi
 
-source /turboarch-config/config
+source /archify-config/config
 
 echo -e "\e[1m\e[46m\e[97mPERFORMING BASIC CONFIGURATION\e[0m"
 ln -sf "/usr/share/zoneinfo/$LOCALTIME" /etc/localtime
 hwclock --systohc
 echo "en_US.UTF-8 UTF-8" >> /etc/locale.gen
 echo "LANG=en_US.UTF-8" > /etc/locale.conf
+locale-gen
 echo "$NEWHOSTNAME" > /etc/hostname
 
+if [[ -n "$NEW_NONSPACE_PASSWORD" ]]; then
+  echo "root:$NEW_NONSPACE_PASSWORD" | chpasswd
+fi
+
 if [ "$SET_SPACE_PASSWORD" -eq 1  ]; then
-     echo "root: " | chpasswd
+  echo "root: " | chpasswd
 fi
 
 rm -rf /boot/*
 
-
-# установить ЕБУЧИЙ СРАКУТ и хрюки пекмена для него
-if [ "$SRAKUT" -eq 1  ]; then
+# install DRACUT and pacman hooks for it
+if [ "$DRACUT" -eq 1  ]; then
   echo -e "\e[1m\e[40m\e[93mINSTALLING DRACUT AND LVM2\e[0m"
-  pacman --noconfirm -Sy lvm2 mdadm dracut
+  pacman --noconfirm -S lvm2 mdadm dracut
 
   echo -e "\e[1m\e[40m\e[93mINSTALL DRACUT HOOKS\e[0m"
 
-  install -Dm644 /turboarch-config/90-dracut-install.hook /usr/share/libalpm/hooks/90-dracut-install.hook
-  install -Dm644 /turboarch-config/60-dracut-remove.hook /usr/share/libalpm/hooks/60-dracut-remove.hook
-  install -Dm755 /turboarch-config/dracut-install /usr/share/libalpm/scripts/dracut-install
-  install -Dm755 /turboarch-config/dracut-remove /usr/share/libalpm/scripts/dracut-remove
+  install -Dm644 /archify-config/90-dracut-install.hook /usr/share/libalpm/hooks/90-dracut-install.hook
+  install -Dm644 /archify-config/60-dracut-remove.hook /usr/share/libalpm/hooks/60-dracut-remove.hook
+  install -Dm755 /archify-config/dracut-install /usr/share/libalpm/scripts/dracut-install
+  install -Dm755 /archify-config/dracut-remove /usr/share/libalpm/scripts/dracut-remove
   
   echo -e "\e[1m\e[40m\e[93mINSTALL KERNEL AND RUN DRACUT HOOKS\e[0m"
   pacman --noconfirm -Rns mkinitcpio
 fi
 
-pacman --noconfirm -Sy linux linux-firmware
+pacman --noconfirm -S linux linux-firmware
 
+if [[ $GRAPHICS_DRIVER != "no" ]]; then
+  echo -e "\e[1m\e[46m\e[97mINSTALLING GRAPHICS DRIVER\e[0m"
+  if [[ "$GRAPHICS_DRIVER" == "amd" ]]; then
+    pacman --noconfirm -S libva-mesa-driver mesa vulkan-radeon xf86-video-amdgpu xf86-video-ati xorg-server xorg-xinit
+  elif [[ "$GRAPHICS_DRIVER" == "intel" ]]; then
+    pacman --noconfirm -S intel-media-driver libva-intel-driver mesa vulkan-intel xorg-server xorg-xinit
+  elif [[ "$GRAPHICS_DRIVER" == "nouveau-nvidia" ]]; then
+    pacman --noconfirm -S libva-mesa-driver mesa vulkan-nouveau xf86-video-nouveau xorg-server xorg-xinit
+  elif [[ "$GRAPHICS_DRIVER" == "nvidia" ]]; then
+    pacman --noconfirm -S libva-nvidia-driver nvidia xorg-server xorg-xinit
+  elif [[ "$GRAPHICS_DRIVER" == "nvidia-open" ]]; then
+    pacman --noconfirm -S libva-nvidia-driver nvidia-open xorg-server xorg-xinit
+  elif [[ "$GRAPHICS_DRIVER" == "virt-machine" ]]; then
+    pacman --noconfirm -S mesa xf86-video-vmware xorg-server xorg-xinit
+  else
+    pacman --noconfirm -S intel-media-driver libva-intel-driver libva-mesa-driver mesa vulkan-intel vulkan-nouveau vulkan-radeon xf86-video-amdgpu xf86-video-ati xf86-video-nouveau xf86-video-vmware xorg-server xorg-xinit
+  fi
+fi
 
-if [ "$GNOME" -eq 1  ]; then
-  echo -e "\e[1m\e[46m\e[97mINSTALLING GNOME\e[0m"
-  pacman --noconfirm -Sy gnome gnome-tweaks
-  systemctl enable gdm
+if [[ $SOUND_SERVER != "no" ]]; then
+  echo -e "\e[1m\e[46m\e[97mINSTALLING SOUND SERVER\e[0m"
+  if [[ "$SOUND_SERVER" == "pipewire" ]]; then
+    pacman --noconfirm -S pipewire
+  elif [[ "$SOUND_SERVER" == "pulseaudio" ]]; then
+    pacman --noconfirm -S pulseaudio
+  fi
+fi
 
-  # удалить ебаные темы колхозанские блять и прочий мусор от старой системы
+if [[ $GRAPHIC != "no" ]]; then
+
+  # delete themes and other garbage from the old system
   for d in /home/*/ ; do
     echo "$d"
     rm -rf "$d/.config/dconf"
@@ -75,12 +103,77 @@ if [ "$GNOME" -eq 1  ]; then
     rm -rf "$d/.themes"
     rm -rf "$d/.icons"
   done
+
+  pacman --noconfirm -S htop iwd nano openssh smartmontools wget wireless_tools man-db man-pages
+
+  echo -e "\e[1m\e[46m\e[97mINSTALLING GRAPHICAL ENVIRONMENT\e[0m"
+  if [[ "$GRAPHIC" == "gnome" ]]; then
+    pacman --noconfirm -S gnome gnome-tweaks
+    systemctl enable gdm
+  elif [[ "$GRAPHIC" == "kde" ]]; then
+    pacman --noconfirm -S plasma plasma-meta plasma-workspace kio-admin spectacle gwenview kcalc sddm kde-applications
+    systemctl enable sddm
+  elif [[ "$GRAPHIC" == "xfce4" ]]; then
+    pacman --noconfirm -S xfce4 xfce4-goodies xarchiver pavucontrol gvfs lightdm lightdm-gtk-greeter
+    systemctl enable lightdm
+  elif [[ "$GRAPHIC" == "awesome" ]]; then
+    pacman --noconfirm -S alacritty awesome feh gnu-free-fonts slock terminus-font ttf-liberation xorg-server xorg-xinit xorg-xrandr xsel xterm gdm
+    systemctl enable gdm
+  elif [[ "$GRAPHIC" == "bspwm" ]]; then
+    pacman --noconfirm -S bspwm dmenu rxvt-unicode sxhkd xdo lightdm lightdm-gtk-greeter
+    systemctl enable lightdm
+  elif [[ "$GRAPHIC" == "budgie" ]]; then
+    pacman --noconfirm -S arc-gtk-theme budgie mate-terminal nemo papirus-icon-theme lightdm lightdm-gtk-greeter
+    systemctl enable lightdm
+  elif [[ "$GRAPHIC" == "cinnamon" ]]; then
+    pacman --noconfirm -S blueman bluez-utils cinnamon engrampa gnome-keyring gnome-screenshot gnome-terminal gvfs-smb system-config-printer xdg-user-dirs-gtk xed lightdm lightdm-gtk-greeter
+    systemctl enable lightdm
+  elif [[ "$GRAPHIC" == "cutefish" ]]; then
+    pacman --noconfirm -S cutefish noto-fonts sddm
+    systemctl enable sddm
+  elif [[ "$GRAPHIC" == "deepin" ]]; then
+    pacman --noconfirm -S deepin deepin-editor deepin-terminal lightdm lightdm-gtk-greeter
+    systemctl enable lightdm
+  elif [[ "$GRAPHIC" == "enlightenment" ]]; then
+    pacman --noconfirm -S englightenment terminology lightdm lightdm-gtk-greeter
+    systemctl enable lightdm
+  elif [[ "$GRAPHIC" == "hyprland" ]]; then
+    pacman --noconfirm -S dolphin dunst grim hyprland kitty polkit-kde-agent qt5-wayland qt6-wayland slurp wofi xdg-desktop-portal-hyprland xdg-utils sddm
+    systemctl enable sddm
+  elif [[ "$GRAPHIC" == "lxqt" ]]; then
+    pacman --noconfirm -S breeze-icons leafpad lxqt oxygen-icons slock ttf-freefont xdg-utils sddm
+    systemctl enable sddm
+  elif [[ "$GRAPHIC" == "mate" ]]; then
+    pacman --noconfirm -S mate mate-extra lightdm lightdm-gtk-greeter
+    systemctl enable lightdm
+  elif [[ "$GRAPHIC" == "qtile" ]]; then
+    pacman --noconfirm -S alacritty qtile lightdm lightdm-gtk-greeter
+    systemctl enable lightdm
+  elif [[ "$GRAPHIC" == "sway" ]]; then
+    pacman --noconfirm -S brightnessctl dmenu foot grim pavucontrol polkit slurp sway swaybg swayidle swaylock waybar xorg-xwayland xdg-utils lightdm lightdm-gtk-greeter
+    systemctl enable lightdm
+  elif [[ "$GRAPHIC" == "i3wm" ]]; then
+    pacman --noconfirm -S dmenu i3-wm i3blocks i3lock i3status lightdm lightdm-gtk-greeter xss-lock xterm
+    systemctl enable lightdm
+  elif [[ "$GRAPHIC" == "fluxbox" ]]; then
+    pacman --noconfirm -S fluxbox xterm xorg-server xorg-xinit
+  elif [[ "$GRAPHIC" == "xorg" ]]; then
+    pacman --noconfirm -S xorg-server xorg-xinit 
+  fi
 fi
 
 if [ "$NETWORKMANAGER" -eq 1  ]; then
   echo -e "\e[1m\e[46m\e[97mINSTALLING NETWORKMANAGER\e[0m"
-  pacman --noconfirm -Sy networkmanager
+  pacman --noconfirm -S networkmanager
   systemctl enable NetworkManager
+fi
+
+CPU_VENDOR=$( sed -n '/vendor_id/{s/^[^:]*: *//;p;q}' /proc/cpuinfo )
+
+if [[ "$CPU_VENDOR" == "GenuineIntel" ]]; then
+  pacman --noconfirm -S intel-ucode
+elif [[ "$CPU_VENDOR" == "AuthenticAMD" ]]; then
+  pacman --noconfirm -S amd-ucode
 fi
 
 echo "GRUB_DISABLE_OS_PROBER=false" >> /etc/default/grub
@@ -102,8 +195,7 @@ else
     read -p "Enter drive for GRUB installation (e.g. sda or nvme0n1): " -r target
 
   fi
-  #echo -e "\e[1m\e[46m\e[97mWIPING MBR FROM TARGET DRIVE: \e[0m" 
-  #dd if=/dev/zero of=/dev/sdx bs=446 count=1
+
 
   echo -e "\e[1m\e[46m\e[97mINSTALLING GRUB (BIOS) ON $target\e[0m" 
   env -i grub-install --target=i386-pc "/dev/$target"
